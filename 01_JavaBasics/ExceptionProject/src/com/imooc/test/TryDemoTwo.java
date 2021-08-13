77fd6e68dca85de5d9948d4b7190901f89864ee9
Chen Zhou <chenzhoux93@gmail.com>
2021-08-12 23:18:24 -0400

    Exception Handling

--- 01_JavaBasics/ExceptionProject/src/com/imooc/test/TryDemoTwo.java
+++ 01_JavaBasics/ExceptionProject/src/com/imooc/test/TryDemoTwo.java
@@ -3,15 +3,13 @@ package com.imooc.test;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class TryDemoOne {
-
+public class TryDemoTwo {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		int one = 12;
-//		int two = 2;
-//		System.out.println("one/two" + (one/two));
-		
-		//接受键盘输入
+		int result = test();
+		System.out.println(result);
+	}
+	
+	public static int test() {
 		Scanner input = new Scanner(System.in);
 		System.out.println("======运算开始======");
 		try {
@@ -19,21 +17,13 @@ public class TryDemoOne {
 			int one = input.nextInt();
 			System.out.print("请输入第一个整数: ");
 			int two = input.nextInt();
-			System.out.println("one/two = " + (one/two));
+			return one/two;
 		}catch(ArithmeticException e) {
-			System.exit(1);
 			System.out.println("除数不允许为0");
-			e.printStackTrace();
-		}catch(InputMismatchException e) {
-			System.out.println("请输入整数");
-			e.printStackTrace();
-		}catch(Exception e) {//捕捉前面异常中无法捕获的信息
-			System.out.println("出错");
-			e.printStackTrace();
+			return 0;
 		}finally {
 			System.out.println("======运算结束======");
+			//return -100000;
 		}
-
 	}
-
 }
