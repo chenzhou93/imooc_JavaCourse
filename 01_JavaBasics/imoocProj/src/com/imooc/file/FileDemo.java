1a1d12f4b5c10f0487b624138126b475824ca8ac
Chen Zhou <chenzhoux93@gmail.com>
2021-12-17 00:41:54 -0700

    File IO

---
+++ 01_JavaBasics/imoocProj/src/com/imooc/file/FileDemo.java
@@ -0,0 +1,37 @@
+package com.imooc.file;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileDemo {
+
+	public static void main(String[] args) {
+		File file1 = new File("c:\\imooc\\io\\score.txt");
+		
+		//File file1 = new File("c:\\imooc", "io\\score.txt");
+//		File file = new File("c:\\imooc");
+//		File file1 = new File(file, "io\\score.txt");
+		
+		System.out.println("is directory: " + file1.isDirectory());
+		System.out.println("is file: " + file1.isFile());
+		
+		//Create Directory
+		File file2 = new File("c:\\imooc\\set", "HashSet");
+		if(!file2.exists()) {
+			file2.mkdirs();
+		}
+		
+		//Create File
+		if(!file1.exists()) {
+			try {
+				file1.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+
+	}
+
+}
