6554192aa73456a636bfab512b1bba430da3cee1
Chen Zhou <chenzhoux93@gmail.com>
2022-11-01 23:18:02 -0400

    Servlet

--- 02_JavaWeb/03_JavaWeb/JavaWeb/FirstServlet/src/com/imooc/servlet/FirstServlet.java
+++ 02_JavaWeb/03_JavaWeb/JavaWeb/FirstServlet/src/com/imooc/servlet/FirstServlet.java
@@ -3,12 +3,26 @@ package com.imooc.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class FirstServlet extends HttpServlet{
+	public FirstServlet() {
+		System.out.println("creating servlet instance");
+	}
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("initializing servlet instance");
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("destroying servlet instance");
+	}
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
