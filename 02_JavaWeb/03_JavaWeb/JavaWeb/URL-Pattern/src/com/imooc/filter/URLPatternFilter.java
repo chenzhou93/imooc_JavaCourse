8f87643c2d1464f3c370a1418150e033ecb4565f
Chen Zhou <chenzhoux93@gmail.com>
2023-03-08 23:30:21 -0500

    Define Filter

---
+++ 02_JavaWeb/03_JavaWeb/JavaWeb/URL-Pattern/src/com/imooc/filter/URLPatternFilter.java
@@ -0,0 +1,26 @@
+package com.imooc.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class URLPatternFilter implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse res = (HttpServletResponse)response;
+		System.out.println("caught: " + req.getRequestURL());
+		chain.doFilter(req, res);
+		
+	}
+
+}
